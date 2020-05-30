20273fd6cb0a9acfbba8bee9016f61b80e5e00c6
yoyoyonoyo <you.itwork@gmail.com>
2020-05-30 19:57:05 +0900

    update signup of devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :text, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   # has_many :user_groups
   # has_many :groups, through: :user_groups
   # has_many :messages
