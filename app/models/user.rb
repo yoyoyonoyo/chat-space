c413947e4746ca988436a22b4eab3d1515eca1be
yoyoyonoyo <you.itwork@gmail.com>
2020-05-30 15:58:09 +0900

    add user.rb association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :text, presence: true, uniqueness: true
+  # has_many :user_groups
+  # has_many :groups, through: :user_groups
+  # has_many :messages
 end
