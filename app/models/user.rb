328ef48cecf92b83138dd08085689a382a184ae0
yoyoyonoyo <you.itwork@gmail.com>
2020-06-01 19:30:03 +0900

    update model&group

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   # has_many :groups, through: :user_groups
   # has_many :messages
 
-  has_many　:group_users
-  has_many :groups,through: :group_users
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
